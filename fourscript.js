d1cd8c6c4ec16fe46f671acfd8ee2db0837890e2
Denise <verdugodenise227@gmail.com>
2026-04-23 13:52:25 -0400

    Update fourscript.js

--- fourscript.js
+++ fourscript.js
@@ -11,30 +11,30 @@ function loadDoc(url,func){
     xhttp.send();
 }
 
-function upload_picture(){
+function upload_picture_public(){
     let xhttp=new XMLHttpRequest();
     xhttp.onload= function(){
         if (xhttp.status != 200){
             console.log("Error");
         }else{
-            upload_file_response(xhttp.response);
+            upload_file_response_public(xhttp.response);
         }
     }
-    xhttp.open("POST", "/uploadpicture", true);
+    xhttp.open("POST", "/uploadpicturepublic", true);
 
     var formData=new FormData();
     formData.append("file", document.getElementById("file").files[0]);
-    formData.append("caption", document.getElementById("caption").value)
+    formData.append("caption", document.getElementById("caption").value);
     xhttp.send(formData);
 }
-function upload_file_response(response){
+function upload_file_response_public(response){
     location.reload();
 }
 //////////////////
-function listpictures(){
-    loadDoc("/listpictures", listpictures_response);
+function listpicturespublic(){
+    loadDoc("/listpicturespublic", listpictures_response_public);
 }
-function listpictures_response(response){
+function listpictures_response_public(response){
     let data= JSON.parse(response);
     let items=data["results"];
     let divResults= document.getElementById("divResults");
